41ba30957ad1c004fab93d93c35bad285d28caad
Andron96 <drandarik2@gmail.com>
2023-12-13 16:33:26 +0200

    2

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
 <?php
 declare(strict_types=1);
+
+function foo(int $a, int $b, closure $c = NULL): float
+{
+    if (is_null($c)) {
+        return $a * $b;
+    }
+    $c($a * $b);
+    return $a * $b;
+}
+
+//$a = 4;
+//$b = 5;
+$a = (int) readline("Введите первое число: ");
+$b = (int) readline("Введите второе число: ");
+$showDecision = (int) readline("Введите \"1\", что бы показать результат вычисления и \"0\", что бы нет: ");
+if ($showDecision ==)
+//$result = foo($a, $b, function ($arg) {
+//    echo "Результат умножения: $arg";
+//});
+//$result = foo($a, $b);
+echo $result;
